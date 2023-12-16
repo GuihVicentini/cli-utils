315fce169fc75214948be9f7cc0bf0a67db55c33
guihvicentini <guilherme.h.vicentini@hotmail.com>
2023-12-16 15:42:32 +0100

    fix: compressing files ci step

--- tar-folders.sh
+++ tar-folders.sh
@@ -6,8 +6,11 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-# Iterate through each folder in the specified directory
-for folder in "$1"/*/; do
+# Change to the specified directory
+cd "$1" || exit 1
+
+# Iterate through each subdirectory
+for folder in */; do
     folder_name=$(basename "$folder")
-    tar czf "${folder_name}.tar.gz" "$folder_name" && echo "Created ${folder_name}.tar.gz"
+    tar -czf "${folder_name}.tar.gz" "$folder_name" && echo "Created ${folder_name}.tar.gz"
 done
