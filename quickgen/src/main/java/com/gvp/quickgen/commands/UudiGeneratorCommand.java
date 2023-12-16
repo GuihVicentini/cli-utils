51743fc64648db9976d46d86c8f7678ce77805c4
guihvicentini <guilherme.h.vicentini@hotmail.com>
2023-12-16 15:20:12 +0100

    feat: generate random uuid command implemented

--- quickgen/src/main/java/com/gvp/quickgen/commands/UudiGeneratorCommand.java
+++ quickgen/src/main/java/com/gvp/quickgen/commands/UudiGeneratorCommand.java
@@ -2,6 +2,8 @@ package com.gvp.quickgen.commands;
 
 import picocli.CommandLine;
 
+import java.util.UUID;
+
 @CommandLine.Command(name = "uuid", mixinStandardHelpOptions = true)
 public class UudiGeneratorCommand implements Runnable {
 
@@ -10,6 +12,30 @@ public class UudiGeneratorCommand implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("some random uuid");
+        int amount = validateAmount(this.amount);
+        for (int i = 0; i < amount; i++) {
+            UUID uuid = generateUuid();
+            printUuidInformation(uuid);
+        }
+    }
+
+    private void printUuidInformation(UUID uuid) {
+        System.out.println(uuid.toString());
+    }
+
+    private UUID generateUuid() {
+        return UUID.randomUUID();
+    }
+
+    private int validateAmount(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Only positive numbers are allowed");
+        }
+
+        if (amount == 0) {
+            return 1;
+        }
+
+        return amount;
     }
 }
