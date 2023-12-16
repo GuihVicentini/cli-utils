7ba65e346c851e3d9d735850578da8b0d8d3dd36
guihvicentini <guilherme.h.vicentini@hotmail.com>
2023-12-16 14:14:59 +0100

    ci: tar downloaded folders

---
+++ tar-folders.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if a directory path is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
+# Iterate through each folder in the specified directory
+for folder in "$1"/*/; do
+    folder_name=$(basename "$folder")
+    tar -czf "${folder_name}.tar.gz" "$folder_name"
+    echo "Created ${folder_name}.tar.gz"
+done
